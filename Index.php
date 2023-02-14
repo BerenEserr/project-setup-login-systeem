be51f2901133d0cfbcb8eec2b936c5a5c927307d
BerenEserr <124245089+BerenEserr@users.noreply.github.com>
2023-02-14 11:28:32 +0100

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<h1> Game pagina </h1>
+
+</head>
+
+<body>
+    Login Pagina
+
+
+    <div id = "frm">  
+        <h1>Login</h1>  
+        <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">  
+            <p>  
+                <label> UserName: </label>  
+                <input type = "text" id ="user" name  = "user" />  
+            </p>  
+            <p>  
+                <label> Password: </label>  
+                <input type = "password" id ="pass" name  = "pass" />  
+            </p>  
+            <p>     
+                <input type =  "submit" id = "btn" value = "Login" />  
+            </p>  
+        </form>  
+    </div>  
+</body>
+
+
+
+
+
+</html>
