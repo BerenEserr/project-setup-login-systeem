feb8144fd3ae68608bad5561a3c9385ae0a32182
BerenEserr <124245089+BerenEserr@users.noreply.github.com>
2023-03-07 11:26:34 +0100

    Add files via upload

--- Index.php
+++ Index.php
@@ -5,12 +5,14 @@
 </head>
 
 <body>
+
+
     Login Pagina
 
 
     <div id = "frm">  
         <h1>Login</h1>  
-        <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">  
+        
             <p>  
                 <label> UserName: </label>  
                 <input type = "text" id ="user" name  = "user" />  
